03aca426d10d19a029a2c8b6ddbb6dd677e20ca6
wagnel <wagnelcuello@gmail.com>
2018-12-23 10:41:14 -0400

    terminacion del ejemplo

--- Proyecto4_Diplomado_Web_MVC_UASD_Registro/Controllers/HomeController.cs
+++ Proyecto4_Diplomado_Web_MVC_UASD_Registro/Controllers/HomeController.cs
@@ -1,7 +1,10 @@
-﻿using System;
+﻿using Proyecto4_Diplomado_Web_MVC_UASD_Registro.Models;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
+using System.Web.Hosting;
 using System.Web.Mvc;
 
 namespace Proyecto4_Diplomado_Web_MVC_UASD_Registro.Controllers
@@ -11,6 +14,28 @@ namespace Proyecto4_Diplomado_Web_MVC_UASD_Registro.Controllers
         // GET: Home
         public ActionResult Index()
         {
+            return View();
+        }
+
+        public ActionResult FormularioVisita()
+        {
+            return View();
+        }
+
+        public ActionResult CargaDatos()
+        {
+            string nombre = Request.Form["nombre"].ToString();
+            string comentarios = Request.Form["comentarios"].ToString();
+            LibroVisitas libro = new LibroVisitas();
+            libro.Grabar(nombre, comentarios);
+            return View();
+        }
+
+        public ActionResult ListadoVisitas()
+        {
+            LibroVisitas libro = new LibroVisitas();
+            string todo = libro.Leer();
+            ViewData["libro"] = todo;
             return View();
         }
     }
---
+++ Proyecto4_Diplomado_Web_MVC_UASD_Registro/Models/LibroVisitas.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Hosting;
+
+namespace Proyecto4_Diplomado_Web_MVC_UASD_Registro.Models
+{
+    public class LibroVisitas
+    {
+        public void Grabar(string nombre, string comentarios)
+        {
+            StreamWriter archivo = new StreamWriter(HostingEnvironment.MapPath("~") + "App_Data/Datos.txt", true);
+            archivo.WriteLine("Nombre: " + nombre + "<br>Comentarios: " + comentarios + "<hr>");
+            archivo.Close();
+        }
+
+        public string Leer()
+        {
+            StreamReader archivo = new StreamReader(HostingEnvironment.MapPath("~") + "/App_Data/datos.txt");
+            string dato = archivo.ReadToEnd();
+            archivo.Close();
+            return dato;
+        }
+    }
+}
