fa3d34e9b79dd238038bbcb80d02084f304b3aeb
wagnel <wagnelcuello@gmail.com>
2018-12-23 10:11:00 -0400

    inicio

---
+++ Proyecto4_Diplomado_Web_MVC_UASD_Registro/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Proyecto4_Diplomado_Web_MVC_UASD_Registro.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
